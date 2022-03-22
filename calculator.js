9dcf7e210b950382696cc745c1e6d9fd37345eab
miketroianello <mike@Mikes-MacBook-Pro.local>
2022-03-22 11:23:14 -0400

    Passed Tests

--- calculator.js
+++ calculator.js
@@ -1,13 +1,26 @@
-function sum(a, b) {
-  return;
+// function sum(a, b) {
+//   if (!a && !b) {
+//     return 0;
+//   } else if (!b) {
+//     return a;
+//   } else {
+//     return a + b;
+//   }
+// }
+
+function sum(a = 0, b = 0) {
+  return a + b;
 }
 
-function subtract(a, b) {
-  return;
+function subtract(a = 0, b = 0) {
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0) {
+    throw new Error();
+  }
+  return a / b;
 }
 
 function multiply(a, b) {
